c8ff57548373dfc65899808a7fdd289007d7258d
am-goku <amkrishna2483@gmail.com>
2024-02-28 19:20:39 +0530

    updated with build

---
+++ build/firebase-messaging-sw.js
@@ -0,0 +1,40 @@
+/* eslint-disable */
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
+
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/service-worker.js", { scope: "/" })
+    .then(function (registration) {
+    })
+    .catch(function (error) {
+      console.error("Service Worker registration failed:", error);
+    });
+}
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCZaocx3wUNHEYz57sWwRgbC3a8OM86k4k",
+  appId: "1:724672213776:web:72c96042da07927c5aeb95",
+  projectId: "relink-app",
+  messagingSenderId: "724672213776",
+});
+
+const initMessaging = firebase.messaging();
+
+self.addEventListener("push", function (event) {
+  const options = JSON.parse(event.data.text());
+
+  const newNotify = JSON.parse(options?.data.newData);
+
+  console.log(newNotify);
+
+  event.waitUntil(
+    self.registration.showNotification("Relink App", {
+      body: `${newNotify?.fromUser} ${newNotify?.message}`,
+    })
+  );
+
+
+});
+
--- src/const/url.js
+++ src/const/url.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:4000";
+// export const BASE_URL = "http://localhost:4000";
 
 //for server
-// export const BASE_URL = "https://relink.thetrendly.shop";
+export const BASE_URL = "https://relink.thetrendly.shop";
