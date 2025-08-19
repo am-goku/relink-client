07d863ba4ca1f316704b10a3ee7c7a2f2c5cf354
am-goku <amkrishna2483@gmail.com>
2025-08-19 16:33:28 +0530

    chore: added reference to the  chat list modaL

--- src/components/chat/ChatList.jsx
+++ src/components/chat/ChatList.jsx
@@ -135,6 +135,7 @@ function ChatList({setReciever}) {
                 Start a new chat
               </h3>
               <button
+                ref={modalDiv}
                 type="button"
                 className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
                 data-modal-hide="medium-modal"
